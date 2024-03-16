456d61f6ef5e7b18545d0daf0e1b4a6a3bc3a988
Stanislav Churilov <st@rk_stas@mail.ru>
2024-03-16 23:01:35 +0300

    add test project jobers

---
+++ js/script.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+    $('.droplist__selected').click(function () { 
+        $(this).closest('.droplist').toggleClass('droplist_active');
+    });
+    $('.droplist__item').click(function () {
+        let currentVal = $(this).text();
+        if($(this).closest('.droplist').hasClass('droplist__select') || $(this).closest('.droplist').hasClass('droplist__check'))
+        $(this).closest('.droplist').children('.droplist__result').addClass('droplist__result_active');
+       if($(this).closest('.droplist').hasClass('droplist__select')) {
+        $(this).closest('.droplist').find('.droplist__result').children('.droplist__result_text').text(currentVal);
+       }
+       if($(this).closest('.droplist').hasClass('droplist__check')) {
+        let currentValChecked = $('.check:checked').map(function () {
+            return $(this).val();
+        }).get().join(', ');
+        $(this).closest('.droplist').find('.droplist__result').children('.droplist__result_text').text(currentValChecked);
+        if (currentValChecked == 0) {
+            $(this).closest('.droplist').find('.droplist__result').children('.droplist__result_text').text('Ни чего не выбрано');
+            $(this).closest('.droplist').find('.droplist__result').removeClass('droplist__result_active');
+        }
+       }
+        
+        $(this).addClass('droplist__item_active').siblings().removeClass('droplist__item_active');
+        
+    });
+    $('.droplist__icon_cross').click(function () { 
+        $(this).siblings('.droplist__result_text').text('Ни чего не выбрано');
+        $(this).closest('.droplist__result').removeClass('droplist__result_active');
+        $(this).closest('.droplist').find('.droplist__item').removeClass('droplist__item_active');
+        $(this).closest('.droplist').find('.droplist__field').val('');
+    });
+
+    $('.droplist__field').keyup(function () {
+        let currentFromVal = $('.droplist__field_from').val();
+        let currentToVal = $('.droplist__field_to').val();
+        let fromValResult = 'от ' + currentFromVal.replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+        let toValResult = ' до ' + currentToVal.replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+        if($('.droplist__field_from').val()) {
+            $(this).closest('.droplist').children('.droplist__result').addClass('droplist__result_active');
+            $(this).closest('.droplist').find('.droplist__result').children('.droplist__result_text').text(fromValResult);
+        }
+        if($('.droplist__field_from').val()  !== '' && $('.droplist__field_to').val()  !== '') {
+            $(this).closest('.droplist').children('.droplist__result').addClass('droplist__result_active');
+            $(this).closest('.droplist').find('.droplist__result').children('.droplist__result_text').text(fromValResult + toValResult);
+        }
+        if($('.droplist__field').val() == '') {
+            $(this).closest('.droplist').children('.droplist__result').removeClass('droplist__result_active');
+            $(this).closest('.droplist').find('.droplist__result').children('.droplist__result_text').text('Ни чего не выбрано');
+        }
+        
+    });
+    $('.droplist__item_hide').click(function () { 
+       if($(this).children('.check').is(':checked')) {
+        $(this).siblings('.droplist__item').fadeOut();
+        $(this).closest('.droplist').find('.droplist__result').fadeOut();
+       }
+       else {
+        $(this).siblings('.droplist__item').fadeIn();
+        $(this).closest('.droplist').find('.droplist__result').fadeIn();
+       }  
+    });
+});
