2c7778312db5299c76618d1320dfe9630cfb78ea
Stanislav Churilov <st@rk_stas@mail.ru>
2024-03-23 16:52:01 +0300

    jobers-vacancy 22.03.2024

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
-    
-   
+    $('.vacancy__switch_button').click(function (e) { 
+        $(this).addClass('active').siblings().removeClass('active');
+        $('.vacancy__statistics_view').removeClass('view_active');
+        $('.vacancy__statistics_view').eq($(this).data('tab')).addClass('view_active');
+    });
 });
